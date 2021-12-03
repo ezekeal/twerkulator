ea949ba8ae3e85787d819e98c23a7a9b05b02fdf
Zach Koch <ezekeal@gmail.com>
2021-12-03 00:31:11 -0600

    add circuit playground files used for the totem

--- device/code.py
+++ device/code.py
@@ -1,114 +1,151 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-# Adafruit Service demo for Adafruit CLUE Circuit Playground Bluefruit board.
-# Accessible via Adafruit Bluefruit Playground app and Web Bluetooth Dashboard.
-
+# """CircuitPython Essentials NeoPixel example"""
 import time
-
 import board
-from digitalio import DigitalInOut
-import neopixel_write
-
-from adafruit_ble import BLERadio
 
 from adafruit_circuitplayground import cp
 
+import neopixel
+
+# from adafruit_led_animation.animation.chase import Chase
+# from adafruit_led_animation.animation.rainbowcomet import RainbowComet
+# from adafruit_led_animation.color import WHITE
+
+from adafruit_ble import BLERadio
 from adafruit_ble_adafruit.adafruit_service import AdafruitServerAdvertisement
-
 from adafruit_ble_adafruit.accelerometer_service import AccelerometerService
-from adafruit_ble_adafruit.addressable_pixel_service import AddressablePixelService
-from adafruit_ble_adafruit.button_service import ButtonService
-from adafruit_ble_adafruit.light_sensor_service import LightSensorService
-from adafruit_ble_adafruit.temperature_service import TemperatureService
-from adafruit_ble_adafruit.tone_service import ToneService
 
+# import displayio
+# import terminalio
+# import adafruit_imageload
+# from adafruit_display_text import label
+# from adafruit_gizmo import tft_gizmo
+
+
+class Strip:
+    def __init__(self, pixels, color, brightness):
+        self.pixels = pixels
+        self.color = color
+        self.brightness = brightness
+
+
+## Lights
+class Strip:
+    def __init__(self, pixels, color, brightness, increasing):
+        self.pixels = pixels
+        self.color = color
+        self.brightness = brightness
+        self.increasing = increasing
+
+
+num_pixels_lg = 35
+num_pixels_small = 19
+red = (255, 0, 0)
+orange = (255, 165, 0)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+purple = (128, 0, 128)
+
+colors = [red, orange, yellow, green, blue, purple]
+
+strips = [
+    Strip(neopixel.NeoPixel(board.A1, num_pixels_lg, brightness=0.1), 0, 0.1, True),
+    Strip(neopixel.NeoPixel(board.A2, num_pixels_lg, brightness=0.1), 1, 0.1, False),
+    Strip(neopixel.NeoPixel(board.A3, num_pixels_lg, brightness=0.1), 2, 0.1, True),
+    Strip(neopixel.NeoPixel(board.A4, num_pixels_small, brightness=0.1), 3, 0.1, False),
+]
+
+# chase = Chase(strips[1], speed=0.1, color=WHITE, size=3, spacing=6)
+# rainbow_comet = RainbowComet(strips[0], speed=0.1, tail_length=7, bounce=False)
+# rainbow_comet2 = RainbowComet(strips[1], speed=0.1, tail_length=7, bounce=False)
+# pixels = neopixel.NeoPixel(board.A1, num_pixels, brightness=0.1, auto_write=False)
+
+
+def animate():
+    for i in range(len(strips)):
+        strip = strips[i]
+        if strip.increasing:
+            if strip.brightness < 1.0:
+                strip.brightness = strip.brightness + 0.1
+            else:
+                strip.increasing = False
+        else:
+            if strip.brightness > 0:
+                strip.brightness = strip.brightness - 0.1
+            else:
+                strip.increasing = True
+                if strip.color < (len(colors) - 1):
+                    strip.color = strip.color + 1
+                else:
+                    strip.color = 0
+        strip.pixels.fill(colors[strip.color])
+        strip.pixels.brightness = strip.brightness
+
+
+## Accelerometer
 accel_svc = AccelerometerService()
 accel_svc.measurement_period = 100
 accel_last_update = 0
 
-# 3 RGB bytes * 10 pixels.
-NEOPIXEL_BUF_LENGTH = 3 * 10
-neopixel_svc = AddressablePixelService()
-neopixel_buf = bytearray(NEOPIXEL_BUF_LENGTH)
-# Take over NeoPixel control from cp.
-cp._pixels.deinit()  # pylint: disable=protected-access
-neopixel_out = DigitalInOut(board.NEOPIXEL)
-neopixel_out.switch_to_output()
 
-button_svc = ButtonService()
-button_svc.set_pressed(cp.switch, cp.button_a, cp.button_b)
-
-light_svc = LightSensorService()
-light_svc.measurement_period = 100
-light_last_update = 0
-
-temp_svc = TemperatureService()
-temp_svc.measurement_period = 100
-temp_last_update = 0
-
-tone_svc = ToneService()
-
+## Bluetooth
 ble = BLERadio()
-# The Web Bluetooth dashboard identifies known boards by their
-# advertised name, not by advertising manufacturer data.
 ble.name = "CPlay"
-
-# The Bluefruit Playground app looks in the manufacturer data
-# in the advertisement. That data uses the USB PID as a unique ID.
-# Adafruit Circuit Playground Bluefruit USB PID:
-# Arduino: 0x8045,  CircuitPython: 0x8046, app supports either
 adv = AdafruitServerAdvertisement()
 adv.pid = 0x8046
 
+
+## Display
+# image_update_rate = 1000
+# # Create the TFT Gizmo display
+# display = tft_gizmo.TFT_Gizmo()
+
+# group = displayio.Group()
+
+# bitmap_file = open("/ai-sprite.bmp", "rb")
+# bitmap = displayio.OnDiskBitmap(bitmap_file)
+
+# ai_grid = displayio.TileGrid(
+#     bitmap,
+#     pixel_shader=bitmap.pixel_shader,
+#     width=1,
+#     height=1,
+#     tile_height=240,
+#     tile_width=240,
+#     default_tile=10,
+#     x=0,
+#     y=0,
+# )
+
+# group.append(ai_grid)
+
+# display.show(group)
+
+# total_frames = 5
+last_update = 0
+# current_frame = 0
+
+
 while True:
-    # Advertise when not connected.
-    ble.start_advertising(adv)
-    while not ble.connected:
-        pass
-    ble.stop_advertising()
+    # strips[0][0] = (255, 0, 0)
+    # rainbow_comet.animate()
+    # rainbow_comet2.animate()
+    if (last_update + 0.07) < time.monotonic():
+        animate()
+        last_update = time.monotonic()
+    #     ai_grid[0] = current_frame
+    #     current_frame += 1
+    #     last_update = time.monotonic()
+    #     if current_frame > (total_frames - 1):
+    #       current_frame = 0
 
-    while ble.connected:
+    if ble.connected and ble.advertising:
+        ble.stop_advertising()
+    elif not ble.connected and not ble.advertising:
+        ble.start_advertising(adv)
+
+    if ble.connected:
         now_msecs = time.monotonic_ns() // 1000000  # pylint: disable=no-member
-
         if now_msecs - accel_last_update >= accel_svc.measurement_period:
             accel_svc.acceleration = cp.acceleration
             accel_last_update = now_msecs
-
-        button_svc.set_pressed(cp.switch, cp.button_a, cp.button_b)
-
-        if now_msecs - light_last_update >= light_svc.measurement_period:
-            light_svc.light_level = cp.light
-            light_last_update = now_msecs
-
-        neopixel_values = neopixel_svc.values
-        if neopixel_values is not None:
-            start = neopixel_values.start
-            if start > NEOPIXEL_BUF_LENGTH:
-                continue
-            data = neopixel_values.data
-            data_len = min(len(data), NEOPIXEL_BUF_LENGTH - start)
-            neopixel_buf[start : start + data_len] = data[:data_len]
-            if neopixel_values.write_now:
-                neopixel_write.neopixel_write(neopixel_out, neopixel_buf)
-
-        if now_msecs - temp_last_update >= temp_svc.measurement_period:
-            temp_svc.temperature = cp.temperature
-            temp_last_update = now_msecs
-
-        tone = tone_svc.tone
-        if tone is not None:
-            freq, duration_msecs = tone
-            if freq != 0:
-                if duration_msecs != 0:
-                    # Note that this blocks. Alternatively we could
-                    # use now_msecs to time a tone in a non-blocking
-                    # way, but then the other updates might make the
-                    # tone interval less consistent.
-                    cp.play_tone(freq, duration_msecs / 1000)
-                else:
-                    cp.stop_tone()
-                    cp.start_tone(freq)
-            else:
-                cp.stop_tone()
-        last_tone = tone
